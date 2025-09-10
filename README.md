a7f1fa68475efe501e41541b7f1ffefbc6ae0368
htpit1106 <phuonggm204@gmail.com>
2025-09-10 10:10:11 +0700

    add readme.md

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Mobile Intern test
+
+
+![Ảnh demo] (./images/img1.jpg)
+![Ảnh demo] (./images/img2.jpg)
+
--- app/src/main/java/com/example/mobileinterntest/MainActivity.kt
+++ app/src/main/java/com/example/mobileinterntest/MainActivity.kt
@@ -4,27 +4,19 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.text.Editable
+
 import android.text.TextWatcher
 import android.widget.EditText
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+
 import com.example.mobileinterntest.Adapter.ResultAdapter
-import com.example.mobileinterntest.ViewModel.MainViewModel
 import com.example.mobileinterntest.ViewModel.SearchViewModel
 import com.example.mobileinterntest.data.model.Place
-import com.example.mobileinterntest.data.repository.SearchRepository
-import com.example.mobileinterntest.ui.theme.MobileInternTestTheme
 
 // implement onclickitem
 class MainActivity : ComponentActivity() , ResultAdapter.setOnClickListener {
@@ -51,8 +43,11 @@ class MainActivity : ComponentActivity() , ResultAdapter.setOnClickListener {
         val layoutManager = LinearLayoutManager(this)
         rvResults.layoutManager = layoutManager
 
+
         edtSearch.addTextChangedListener(object: TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 viewModel.setQuery(s?.toString() ?: "")
                 if (s.isNullOrBlank()) {
@@ -77,7 +72,9 @@ class MainActivity : ComponentActivity() , ResultAdapter.setOnClickListener {
     override fun onRedirectClick(place: Place) {
 
         try{
-            val gmmIntentUri = Uri.parse("google.navigation:q=${place.lat},${place.lon}")
+
+            val gmmIntentUri = Uri.parse("geo:${place.lat},${place.lon}?q=${Uri.encode(place.displayName)}")
+
             val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
             mapIntent.setPackage("com.google.android.apps.maps") // mở bằng Google Maps
             startActivity(mapIntent)
